a18131eba0eb8a0127f8e0a4e09cd0d31652e118
katya-voronova <katya@example.com>
2026-01-15 14:34:47 +0300

    fixing errors tests

--- backend/api/views/recipe_views.py
+++ backend/api/views/recipe_views.py
@@ -3,19 +3,24 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.decorators import action
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.filters import IngredientFilter, RecipeFilter
 from api.permissions import IsOwnerOrAdminOrReadOnly
-from api.serializers import (FavoriteSerializer, IngredientSerializer,
-                             RecipeSerializer, ShoppingListSerializer,
-                             TagSerializer)
+from api.serializers import (
+    FavoriteSerializer,
+    IngredientSerializer,
+    RecipeSerializer,
+    ShoppingListSerializer,
+    TagSerializer,
+)
 from backend.mixins import CreateDeleteMixin
-from recipes.models import (Favorite, Ingredient, Recipe, ShoppingList,
-                            Tag)
+from recipes.models import Favorite, Ingredient, Recipe, ShoppingList, Tag
 from recipes.tasks import generate_shopping_list_text
 
 
@@ -39,50 +44,54 @@ class RecipeViewSet(CreateDeleteMixin, ModelViewSet):
         user = self.request.user
         return (
             Recipe.objects.with_flags(user)
-            .select_related('author')
-            .prefetch_related('tags', 'recipes__ingredient')
+            .select_related("author")
+            .prefetch_related("tags", "recipes__ingredient")
         )
 
     @action(
         detail=False,
-        url_path='download_shopping_cart',
-        permission_classes=(IsAuthenticated,)
+        url_path="download_shopping_cart",
+        permission_classes=(IsAuthenticated,),
     )
     def download_shopping_cart(self, request):
         """Скачать список покупок."""
         task = generate_shopping_list_text.delay(request.user.id)
         content = task.get(interval=0.1)
-        response = HttpResponse(content, content_type='text/plain')
-        response['Content-Disposition'] = 'attachment; filename="shopping_list.txt"'
+        response = HttpResponse(content, content_type="text/plain")
+        response["Content-Disposition"] = (
+            'attachment; filename="shopping_list.txt"'
+        )
         return response
 
-    @action(detail=True, url_path='get-link')
+    @action(detail=True, url_path="get-link")
     def get_link(self, request, pk=None):
         """Получить короткую ссылку на рецепт."""
         recipe = self.get_object()
         short_url = request.build_absolute_uri(
-            reverse('short-link', args=[recipe.pk])
+            reverse("short-link", args=[recipe.pk])
         )
-        return Response({'short-link': short_url})
+        return Response({"short-link": short_url})
 
-    @action(detail=True, methods=('post',))
+    @action(detail=True, methods=("post",))
     def favorite(self, request, pk):
         """Добавить рецепт в избранное."""
-        favorite_data = {'user': request.user.id, 'recipe': pk}
+        favorite_data = {"user": request.user.id, "recipe": pk}
         return self.create_item(
-            Favorite, FavoriteSerializer, favorite_data, request)
+            Favorite, FavoriteSerializer, favorite_data, request
+        )
 
     @favorite.mapping.delete
     def unfavorite(self, request, pk):
         """Удалить рецепт из избранного."""
         return self.delete_item(Favorite, user=request.user, recipe=pk)
 
-    @action(detail=True, methods=('post',), url_path='shopping_cart')
+    @action(detail=True, methods=("post",), url_path="shopping_cart")
     def add_to_cart(self, request, pk):
         """Добавить рецепт в корзину покупок."""
-        cart_data = {'user': request.user.id, 'recipe': pk}
+        cart_data = {"user": request.user.id, "recipe": pk}
         return self.create_item(
-            ShoppingList, ShoppingListSerializer, cart_data, request)
+            ShoppingList, ShoppingListSerializer, cart_data, request
+        )
 
     @add_to_cart.mapping.delete
     def remove_from_cart(self, request, pk):
@@ -93,7 +102,7 @@ class RecipeViewSet(CreateDeleteMixin, ModelViewSet):
 def short_link_redirect(request, pk):
     """Перенаправляет с короткой ссылки на страницу рецепта."""
     recipe = get_object_or_404(Recipe, pk=pk)
-    return redirect('recipes-detail', pk=recipe.pk)
+    return redirect("recipes-detail", pk=recipe.pk)
 
 
 class IngredientViewSet(BaseReadOnlyViewSet):
